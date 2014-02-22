de10a85f65b25e442bc4578763fd8a9224ba8b48
Scott Adie <scottynosabe@gmail.com>
2014-02-22 11:05:28 -0500

    Python 3 compatability

--- tpp.py
+++ tpp.py
@@ -7,6 +7,7 @@
 #Install using:
 #pip install requests
 
+from __future__ import print_function
 import requests
 import time
 import argparse
@@ -17,29 +18,29 @@ _parser.add_argument("--important", action="store_true")
 _args = _parser.parse_args()
 
 _api_url = "http://api.reddit.com/live/sw7bubeycai6hey4ciytwamw3a"
-_headers = {"User-Agent": "TTPLiveConsole/1.0 by sc00ty"}
+_headers = {"User-Agent": "TTPConsole/1.1 by sc00ty"}
 _timeout = 60 #1 Minute
 _last_id = ""
 
 while True:
   try:
-    #Request the json data for the live feed
+    #Request the JSON data for the live feed
     payload = {"before": _last_id}
-    feed_data = requests.get(_api_url, params=payload, headers=_headers).json()
+    feed = requests.get(_api_url, params=payload, headers=_headers).json()
 
     #Iterate backwards through the list, making it so items are shown chronologically
-    for feed_item in feed_data["data"]["children"][::-1]:
+    for feed_item in feed["data"]["children"][::-1]:
       #Store the last seen id
       _last_id = feed_item["data"]["name"]
       body_text = feed_item["data"]["body"]
 
       #If all text should be shown OR only important stuff, and this is important... show the update!
       if not _args.important or ("**" in body_text and _args.important):
-        print "%s\n" % (body_text,)
+        print("%s\n" % (body_text,))
 
     #Progress towards next update.
     for i in range (0, _timeout):
-      print "Checking for update in %ss.\r" % (_timeout - i),
+      print("Checking for update in %ss.\r" % (_timeout - i), end="")
       sys.stdout.flush()
       time.sleep(1)
   except KeyboardInterrupt:
